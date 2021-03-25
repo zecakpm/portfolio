93776da1e3ff9af10275dea0f7520b2e76249f1b
Zeca Formiga <admin@Admins-MacBook-Pro.local>
2021-03-25 06:35:40 +0000

    updates

---
+++ images/naive_bayes/Introduction to Bayesian Inference _ Oracle Data Science_files/backbutton.js
@@ -0,0 +1,13 @@
+(function(factory){if(typeof define==='function'&&define.amd){define(['jquery'],factory)}else if(typeof exports==='object'){factory(require('jquery'))}else{factory(jQuery)}}(function($){var pluses=/\+/g;function encode(s){return config.raw?s:encodeURIComponent(s)}
+function decode(s){return config.raw?s:decodeURIComponent(s)}
+function stringifyCookieValue(value){return encode(config.json?JSON.stringify(value):String(value))}
+function parseCookieValue(s){if(s.indexOf('"')===0){s=s.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,'\\')}
+try{s=decodeURIComponent(s.replace(pluses,' '));return config.json?JSON.parse(s):s}catch(e){}}
+function read(s,converter){var value=config.raw?s:parseCookieValue(s);return $.isFunction(converter)?converter(value):value}
+var config=$.cookie=function(key,value,options){if(value!==undefined&&!$.isFunction(value)){options=$.extend({},config.defaults,options);if(typeof options.expires==='number'){var days=options.expires,t=options.expires=new Date();t.setTime(+t+days*864e+5)}
+return(document.cookie=[encode(key),'=',stringifyCookieValue(value),options.expires?'; expires='+options.expires.toUTCString():'',options.path?'; path='+options.path:'',options.domain?'; domain='+options.domain:'',options.secure?'; secure':''].join(''))}
+var result=key?undefined:{};var cookies=document.cookie?document.cookie.split('; '):[];for(var i=0,l=cookies.length;i<l;i++){var parts=cookies[i].split('=');var name=decode(parts.shift());var cookie=parts.join('=');if(key&&key===name){result=read(cookie,value);break}
+if(!key&&(cookie=read(cookie))!==undefined){result[name]=cookie}}
+return result};config.defaults={};$.removeCookie=function(key,options){if($.cookie(key)===undefined){return!1}
+$.cookie(key,'',$.extend({},options,{expires:-1}));return!$.cookie(key)}}));jQuery(document).ready(function($){dynamicBackbuttons();function dynamicBackbuttons(){$("a.dynamicback").on("click",function(event){var href=$(this).attr('href');var backbuttonurl=$('#backbuttonurl').val();var backbuttontext=$('#backbuttontext').val();if(href&&backbuttonurl&&backbuttontext){event.preventDefault();$.cookie('recentpage',backbuttontext,{path:'/'});$.cookie('recenturl',backbuttonurl,{path:'/'});window.location=$(this).attr('href')}})}
+$('.oracle-simpleheader__button').each(function(index){var recentpage=$.cookie('recentpage');var recenturl=$.cookie('recenturl');if(recentpage!=undefined&&recenturl!=undefined){$(this).html("Back to "+recentpage);$(this).attr("href",recenturl);$(this).show();$('.oracle-simpleheader').addClass('oracle-simpleheader--withheadroom')}else{$(this).hide()}})})
